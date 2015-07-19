726b8352c8a2beab5e545d121008f33213a20c23
GlennRen <glenn.r.phs@gmail.com>
2015-07-19 02:34:55 -0400

    New website

---
+++ README.md
@@ -0,0 +1,2 @@
+# premiere-home-realty
+Hired by Premiere Home Realty to redevelop their new website.
---
+++ scripts/switcher.js
@@ -0,0 +1,59 @@
+function imageBalti() {
+	var img = document.getElementById("image");
+	img.src="images/communities/balti.jpg";
+	return false;
+};
+
+function imageDC() {
+	var img = document.getElementById("image");
+	img.src="images/communities/dc.jpg";
+	return false;
+};
+
+function imageFair() {
+	var img = document.getElementById("image");
+	img.src="images/communities/fair.jpg";
+	return false;
+};
+
+function imageFred() {
+	var img = document.getElementById("image");
+	img.src="images/communities/fred.jpg";
+	return false;
+};
+
+function imageHow() {
+	var img = document.getElementById("image");
+	img.src="images/communities/how.jpg";
+	return false;
+};
+
+function imageLoud() {
+	var img = document.getElementById("image");
+	img.src="images/communities/loud.jpg";
+	return false;
+};
+
+function imageMont() {
+	var img = document.getElementById("image");
+	img.src="images/communities/mont.jpg";
+	return false;
+};
+
+function imagePG() {
+	var img = document.getElementById("image");
+	img.src="images/communities/pg.jpg";
+	return false;
+};
+
+function imagePW() {
+	var img = document.getElementById("image");
+	img.src="images/communities/pw.jpg";
+	return false;
+};
+
+function imageStaff() {
+	var img = document.getElementById("image");
+	img.src="images/communities/staff.jpg";
+	return false;
+};
